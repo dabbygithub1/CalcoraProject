3370720b1552284e95f81e46fac7f1cba5d157fa
vladpashkevich88@gmail.com <145257376+dabbygithub1@users.noreply.github.com>
2025-05-16 19:41:51 +0300

    Создание/изменение новых/старых файлов

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,157 @@
+import './HomePage.css';
+
+const HomePage = () => {
+  	return (
+    		<div className="w-light">
+      			<div className="main">
+        				<div className="section">
+          					<div className="container">
+            						<b className="heading-2">Как это работает</b>
+            						<div className="backgroundbordershadow">
+              							<img className="svg-icon" alt="" src="SVG.svg" />
+              							<div className="heading-3">Зарегистрируйся</div>
+              							<div className="div">
+                								<p className="p">Создайте личный кабинет</p>
+                								<p className="p">для сохранения ваших</p>
+                								<p className="p">расчетов</p>
+              							</div>
+            						</div>
+            						<div className="backgroundbordershadow1">
+              							<img className="svg-icon" alt="" src="SVG.svg" />
+              							<div className="heading-31">Выбери профессию</div>
+              							<div className="div1">
+                								<p className="p">Укажите вашу сферу</p>
+                								<p className="p">деятельности для точного</p>
+                								<p className="p">расчета</p>
+              							</div>
+            						</div>
+            						<div className="backgroundbordershadow2">
+              							<img className="svg-icon" alt="" src="SVG.svg" />
+              							<div className="heading-32">Укажи дни и доход</div>
+              							<div className="div2">
+                								<p className="p">Введите информацию о</p>
+                								<p className="p">рабочих днях и больничных</p>
+              							</div>
+            						</div>
+            						<div className="backgroundbordershadow3">
+              							<img className="svg-icon" alt="" src="SVG.svg" />
+              							<div className="heading-33">Получи расчёт</div>
+              							<div className="div3">
+                								<p className="p">Получите детальный расчет</p>
+                								<p className="p">с учетом всех нюансов</p>
+              							</div>
+            						</div>
+          					</div>
+        				</div>
+        				<div className="section1">
+          					<img className="svg-icon4" alt="" src="SVG.svg" />
+          					<div className="heading-4">Безопасно</div>
+          					<div className="div4">Ваши данные надежно зашифрованы и недоступны третьим лицам</div>
+          					<img className="svg-icon5" alt="" src="SVG.svg" />
+          					<div className="heading-41">Много профессий</div>
+          					<div className="div5">Поддержка различных сфер деятельности и типов занятости</div>
+          					<img className="svg-icon6" alt="" src="SVG.svg" />
+          					<div className="heading-42">История расчётов</div>
+          					<div className="div6">Сохранение всех ваших расчетов с возможностью просмотра</div>
+          					<img className="svg-icon7" alt="" src="SVG.svg" />
+          					<div className="heading-43">Настройка</div>
+          					<div className="div7">Персонализация настроек калькулятора</div>
+        				</div>
+        				<div className="section2">
+          					<div className="container1">
+            						<b className="heading-21">Готовы рассчитать вашу зарплату?</b>
+              							<div className="container2">
+                								<div className="div8">
+                  									<p className="p">Получите точный расчет с учетом всех нюансов вашей профессии, включая</p>
+                  									<p className="p">больничные и отпускные</p>
+                								</div>
+              							</div>
+              							<div className="link">
+                								<div className="div9">Начать бесплатный расчет</div>
+              							</div>
+              							</div>
+              							</div>
+              							<div className="section3">
+                								<div className="heading-1">
+                  									<b className="b">
+                    										<p className="p">Умный расчёт зарплаты</p>
+                    										<p className="p">и больничных</p>
+                  									</b>
+                								</div>
+                								<div className="div10">Быстро. Точно. Для всех профессий</div>
+                								<div className="link1">
+                  									<div className="div11">Начать расчёт</div>
+                								</div>
+                								<div className="background" />
+                								<div className="background1" />
+                								<div className="overlayshadowoverlayblur">
+                  									<div className="background2">
+                    										<img className="svg-icon8" alt="" src="SVG.svg" />
+                    										<div className="background3">
+                      											<div className="background4">
+                        												<b className="b1">$</b>
+                      											</div>
+                    										</div>
+                  									</div>
+                  									<div className="background5" />
+                  									<div className="background6" />
+                  									<div className="background7" />
+                  									<div className="background8" />
+                  									<div className="background9" />
+                								</div>
+              							</div>
+              							</div>
+              							<div className="footer">
+                								<div className="container3">
+                  									<div className="heading-34">Calcora</div>
+                  									<div className="div12">
+                    										<p className="p">Современный сервис для расчёта зарплаты с учетом</p>
+                    										<p className="p">больничных и других факторов.</p>
+                  									</div>
+                  									<div className="heading-35">Контакты</div>
+                  									<div className="list">
+                    										<div className="link2">
+                      											<img className="svg-icon9" alt="" src="SVG.svg" />
+                      											<div className="vladpashkevich88gmailcom">vladpashkevich88@gmail.com</div>
+                    										</div>
+                    										<div className="link3">
+                      											<img className="svg-icon9" alt="" src="SVG.svg" />
+                      											<div className="div13">+375 (29) 237-19-27</div>
+                    										</div>
+                  									</div>
+                  									<div className="calcora">© 2025 Calcora. Все права защищены.</div>
+                								</div>
+              							</div>
+              							<div className="nav">
+                								<b className="link-calcora">Calcora</b>
+                								<div className="link4">
+                  									<div className="div14">Главная</div>
+                								</div>
+                								<div className="link5">
+                  									<img className="svg-icon11" alt="" src="SVG.svg" />
+                  									<div className="div15">Калькулятор</div>
+                								</div>
+                								<div className="link6">
+                  									<img className="svg-icon11" alt="" src="SVG.svg" />
+                  									<div className="div16">Профессии</div>
+                								</div>
+                								<div className="link7">
+                  									<img className="svg-icon11" alt="" src="SVG.svg" />
+                  									<div className="div17">Контакты</div>
+                								</div>
+                								<div className="link8">
+                  									<img className="svg-icon11" alt="" src="SVG.svg" />
+                  									<div className="div18">О нас</div>
+                								</div>
+                								<div className="link9">
+                  									<div className="div19">Войти</div>
+                								</div>
+                								<div className="link10">
+                  									<div className="div20">Зарегистрироваться</div>
+                								</div>
+              							</div>
+              							</div>);
+            						};
+            						
+            						export default HomePage;
+            						
